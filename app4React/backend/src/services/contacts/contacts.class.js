371b081c110e0dc372351d77b04f3ae67efca0bb
apifever <apis.apifever@gmail.com>
2020-02-22 19:50:27 +0100

    Removed unused files

--- app4React/backend/src/services/contacts/contacts.class.js
+++
@@ -1,5 +0,0 @@
-const { Service } = require('feathers-mongoose');
-
-exports.Contacts = class Contacts extends Service {
-  
-};
--- app4React/backend/src/services/index.js
+++
@@ -1,5 +0,0 @@
-const contacts = require('./contacts/contacts.service.js');
-// eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
-  app.configure(contacts);
-};
