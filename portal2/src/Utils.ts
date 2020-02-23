ffd2e2e75d60cc333b44afafcb91a8e92e8b7cdc
apifever <apis.apifever@gmail.com>
2020-02-23 11:42:29 +0100

    Navigation bar clicks working

---
+++ portal2/src/Utils.ts
@@ -0,0 +1,24 @@
+
+
+export function hasClass(el: Element, className: string) {
+    if (el.classList)
+        return el.classList.contains(className);
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
+
+export function addClass(el: Element, className: string) {
+    if (el.classList)
+        el.classList.add(className);
+    else if (!hasClass(el, className))
+        el.className += " " + className;
+}
+
+export function removeClass(el: Element, className: string) {
+    if (el.classList)
+        el.classList.remove(className);
+    else if (hasClass(el, className))
+    {
+        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+        el.className = el.className.replace(reg, ' ');
+    }
+}
--- portal2/src/index.ts
+++ portal2/src/index.ts
@@ -1,31 +1,91 @@
 import './styles/style.scss';
+import * as utils from "./Utils";
 
 console.log('hello, world');
 
 const testMessage: string = 'TypeScript works';
 
-let links = document.getElementsByClassName('navLinks');
-console.log('links', links);
-Array.prototype.filter.call(links, function(ul: any){
-    console.log(ul.nodeName);
-    let lis = ul.getElementsByTagName('LI');
-    Array.prototype.filter.call(lis, function(li: any){
-        console.log(li.nodeName);
-        let as = li.getElementsByTagName('A');
-        Array.prototype.filter.call(as, function(a: any){
-            console.log(a.nodeName);
-            a.addEventListener('click', function (event: any) {
+function getLoadingEl() {
+    return document.getElementsByTagName('LOADING')[0];
+}
 
-                // Don't follow the link
-                event.preventDefault();
-            
-                // Log the clicked element in the console
-                console.log(event.target);
-            
-            }, false);  
-        });    
+function getContentEl() {
+    return document.getElementsByTagName('CONTENT')[0];
+}
+
+function start() {
+    let loadingEl = getLoadingEl();    
+    utils.removeClass(loadingEl, 'show');
+    utils.addClass(loadingEl, 'hide');
+
+    let contentEl = getContentEl();
+    utils.removeClass(contentEl, 'show');
+    utils.addClass(contentEl, 'hide');
+}
+
+function load() {
+    let loadingEl = getLoadingEl();    
+    utils.removeClass(loadingEl, 'hide');
+    utils.addClass(loadingEl, 'show');
+    let contentEl = getContentEl();
+    utils.removeClass(contentEl, 'show');
+    utils.addClass(contentEl, 'hide');
+}
+
+function loaded() {
+    let loadingEl = getLoadingEl();    
+    utils.removeClass(loadingEl, 'show');
+    utils.addClass(loadingEl, 'hide');
+    let contentEl = getContentEl();
+    utils.removeClass(contentEl, 'hide');
+    utils.addClass(contentEl, 'show');
+}
+
+function handleClick(event: any) {
+
+    // Don't follow the link
+    event.preventDefault();
+
+    // Log the clicked element in the console
+    console.log(event.target);
+
+    load();    
+    loaded();
+}
+
+function init() {
+    // Attach handleClicks to all A elements in the navigation
+    let links = document.getElementsByClassName('navLinks');
+    console.log('links', links);
+    Array.prototype.filter.call(links, function(ul: any){
+        console.log(ul.nodeName);
+        let lis = ul.getElementsByTagName('LI');
+        Array.prototype.filter.call(lis, function(li: any){
+            console.log(li.nodeName);
+            let as = li.getElementsByTagName('A');
+            Array.prototype.filter.call(as, function(a: any){
+                console.log(a.nodeName);
+                // Ensure to register click event lietner only once
+                a.removeEventListener("click", handleClick, false);
+                a.addEventListener('click', handleClick, false);  
+            });    
+        });
     });
-});
+
+    // Attach handleClicks to Contact A element
+    let ctaLinks = document.getElementsByClassName('cta');
+    Array.prototype.filter.call(ctaLinks, function(cta: any){
+        console.log(cta.nodeName);
+        // Ensure to register click event lietner only once
+        cta.removeEventListener("click", handleClick, false);
+        cta.addEventListener('click', handleClick, false);  
+    });    
+
+    // Hide loading and content element
+    start();
+}
+
+init();
 
 /*
 links.each(element => {
