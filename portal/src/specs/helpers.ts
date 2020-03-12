9e8317a34d5e6b132781e947f9dcb594c5abb6af
apifever <apis.apifever@gmail.com>
2020-03-12 01:17:08 +0100

    First e2e test implemented

--- portal/src/specs/helpers.ts
+++ portal/src/specs/helpers.ts
@@ -1,4 +1,4 @@
-import { By, until, WebDriver } from 'selenium-webdriver';
+import { By, until, WebDriver, WebElement } from 'selenium-webdriver';
 
 const waitUntilTime = 20000
 
@@ -7,5 +7,37 @@ export async function querySelector(selector: string, driver: WebDriver) {
     until.elementLocated(By.css(selector)),
     waitUntilTime
   )
-  return await driver.wait(until.elementIsVisible(el), waitUntilTime)
+  return el;
 }
+
+export async function waitElementVisible(element: WebElement, driver: WebDriver) {
+  return await driver.wait(until.elementIsVisible(element), waitUntilTime)
+}
+
+export async function querySelectorsAll(selector: string, driver: WebDriver): Promise<WebElement[]> {
+  const els = await driver.wait(
+    until.elementsLocated(By.css(selector)),
+    waitUntilTime
+  )
+  return els;
+}
+
+export async function waitElementsVisible(elements: WebElement[], driver: WebDriver, waitVisible?: boolean): Promise<WebElement[]> {
+  let res: Promise<WebElement>[] = [];
+  elements.forEach((el) => {
+    res.push(driver.wait(until.elementIsVisible(el), waitUntilTime));
+  });    
+  return await Promise.all(res);
+}
+
+export async function getTagNameAndClasses(elements: WebElement[]): Promise<string[]> {
+  let names = elements.map(async (element) => {
+    let a: Promise<string>[] = [];
+    a.push(element.getTagName())
+    a.push(element.getAttribute('class'))
+    let s: string[] = await Promise.all(a);
+    //console.log('s[0]: ', s[0], 's[1]', s[1])
+    return s[0] + '.' + s[1].replace(' ', '.')
+  });
+  return await Promise.all(names);
+} 
--- portal/src/specs/index.spec.ts
+++ portal/src/specs/index.spec.ts
@@ -1,8 +1,8 @@
-import {Builder, WebDriver} from 'selenium-webdriver';
+import {Builder, WebDriver, By} from 'selenium-webdriver';
 import * as helpers from './helpers';
 
-const rootURL = 'https://www.mozilla.org/en-US/'
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
+const rootURL = 'http://localhost:8080';
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5;
 
 //console.log('variables',  process.argv);
 let driverName: string;
@@ -13,7 +13,7 @@ if (process.argv.length > 2) {
   }
 }
 
-describe(`Executing tests with ${driverName} driver.`, () => {
+describe(`Executing tests with [${driverName}] Selenium driver.`, () => {
   let driver: WebDriver;
 
   beforeAll(async () => {
@@ -24,14 +24,50 @@ describe(`Executing tests with ${driverName} driver.`, () => {
     driver.get(rootURL);
   });
 
-  test('should click on navbar button to display a drawer1', async () => {
-    const anchor = await helpers.querySelector('[href=\'/en-US/firefox/\']', driver)
-    const actual = await anchor.getText()
-    const expected = 'Firefox'
-    expect(actual).toEqual(expected)
+  test('Title should be Portal', async () => {
+    const actual = await driver.getTitle();
+    const expected = 'Portal';
+    expect(actual).toEqual(expected);
   });
 
-  test('should click on navbar button to display a drawer2', async () => {
+  test('Redirection from "/" to "/home" worked correctly', async () => {
+      const actual = await driver.getCurrentUrl();
+      const expected = 'http://localhost:8080/#/home';
+      expect(actual).toEqual(expected);
+      let element = await helpers.querySelector('home[class^="portal-page show"]', driver)
+      element = await helpers.waitElementVisible(element, driver)
+      expect(element).toBeTruthy();
+      const elements = await helpers.querySelectorsAll('*[class^="hide"]', driver)
+      expect(elements).toBeTruthy();
+      expect(elements.length).toBeGreaterThanOrEqual(3);
+      let names = await helpers.getTagNameAndClasses(elements);
+      expect(names).toContainEqual('loading.hide')
+      expect(names).toContainEqual('error.hide')
+      expect(names).toContainEqual('content.hide')
+      const elements2 = await helpers.querySelectorsAll('*[class^="portal-page hide"]', driver)
+      expect(elements2).toBeTruthy();
+      expect(elements2.length).toBeGreaterThanOrEqual(3);
+      let names2 = await helpers.getTagNameAndClasses(elements2);
+      expect(names2).toContainEqual('login.portal-page.hide')
+      expect(names2).toContainEqual('contact.portal-page.hide')
+      expect(names2).toContainEqual('protected.portal-page.hide')
+      
+    //document.querySelectorAll('*[class^="hide"]');
+    /*
+    0: loading.hide
+    1: error.hide
+    2: content.hide
+    */
+    //document.querySelectorAll('*[class^="portal-page hide"]');
+    /*
+    0: login.portal-page.hide
+    1: contact.portal-page.hide
+    2: protected.portal-page.hide
+    */
+  });
+
+
+  test.skip('should click on navbar button to display a drawer1', async () => {
     const anchor = await helpers.querySelector('[href=\'/en-US/firefox/\']', driver)
     const actual = await anchor.getText()
     const expected = 'Firefox'
